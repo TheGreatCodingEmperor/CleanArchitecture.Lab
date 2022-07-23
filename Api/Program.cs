6239f1a789c3d120ce9b5d78a8118bdba018e3d3
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2022-07-23 11:48:29 +0800

    1. spa service 2. primeng theme

--- Api/Program.cs
+++ Api/Program.cs
@@ -18,6 +18,11 @@ builder.Services.AddScoped<IInvoiceRepository,InvoiceRepository>();
 builder.Services.AddScoped<IInvoiceItemRepository,InvoiceItemRepository>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddSpaStaticFiles (configuration => {
+    configuration.RootPath = "ClientApp/dist";
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -36,5 +41,21 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSpa (spa => {
+    // To learn more about options for serving an Angular SPA from ASP.NET Core,
+    // see https://go.microsoft.com/fwlink/?linkid=864501
+
+    spa.Options.SourcePath = "ClientApp";
+
+    if (app.Environment.IsDevelopment ()) {
+        // spa.UseAngularCliServer (npmScript: "start"); // IE 11
+        // spa.UseAngularCliServer (npmScript: "start-es6");
+        // spa.Options.StartupTimeout = TimeSpan.FromSeconds (120); // Increase the timeout if angular app is taking longer to startup
+
+        spa.UseProxyToSpaDevelopmentServer ("http://localhost:4200"); // Use this instead to use the angular cli server
+    }
+});
+
 
 app.Run();
